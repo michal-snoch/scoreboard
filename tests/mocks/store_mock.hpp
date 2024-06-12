3c4ff4716a6a84394a44841aee87d116af22cd92
Michal Snoch <michal@snoch.eu>
2024-06-12 09:04:56 +0200

    StoreMock implementation

--- tests/mocks/store_mock.hpp
+++ tests/mocks/store_mock.hpp
@@ -3,7 +3,9 @@
 
 struct StoreMock : Store
 {
+    MOCK_METHOD(void, createOrUpdate, (std::string homeTeam, std::string awayTeam, uint16_t homeScore, uint16_t awayScore), (override));
     MOCK_METHOD(void, remove, (std::string homeTeam, std::string awayTeam), (override));
+    MOCK_METHOD(std::list<Store::Entry>&, getAll, (), (const, override));
 
     virtual ~StoreMock() = default;
 };
