925e672c3c5e10e07b8c94aeb7583964e2287ffe
Michal Snoch <michal@snoch.eu>
2024-06-12 11:34:18 +0200

    Fix for order

--- src/score_board.cpp
+++ src/score_board.cpp
@@ -10,7 +10,7 @@ std::list<std::string> ScoreBoard::getGamesSummary() const
 {
     const auto& storedEntries = store.getAll();
 
-    std::vector<Store::Entry> entries(storedEntries.begin(), storedEntries.end());
+    std::vector<Store::Entry> entries(storedEntries.rbegin(), storedEntries.rend());
     std::stable_sort(entries.begin(), entries.end(), [](const auto& e1, const auto& e2) {
         return (e1.homeScore + e1.awayScore) > (e2.homeScore + e2.awayScore);
     });
